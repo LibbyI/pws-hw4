d509d2ed9eddbf4eb69dd4f8f828e71e7ffa0b89
libby <libby@campus.technion.ac.il>
2024-03-17 17:16:36 +0200

    start with react

--- src/gateway-service/gateway-service.ts
+++ src/gateway-service/gateway-service.ts
@@ -1,5 +1,8 @@
 import express, { Application } from "express";
 import * as dotenv from "dotenv";
+import cors from 'cors';
+import axios from 'axios';
+
 
 
 dotenv.config();
@@ -10,9 +13,26 @@ const port = process.env.GATEWAY_PORT;
 const users_url = process.env.USERS_SERVICE_URL;
 
 
+// TODO: check if cors is allowed
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
 
-app.post('/api/signup', (req, res) => {
-  res.redirect(`${users_url}/signup`);
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+
+app.post('/api/signup', cors(corsOptions) , async(req, res) => {
+  try{
+    const response = await axios.post(`${users_url}/signup`, req.body);
+    console.log(response.status, response.data);
+    res.status(response.status).send(response.data);
+
+  } catch(error){
+    console.log(error.status);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.put('/api/permissions', (req, res) => {
