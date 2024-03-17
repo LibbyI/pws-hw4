9fbf2fa80054540f3c0a13e78b644fd1a59fc9ec
libby <libby@campus.technion.ac.il>
2024-03-17 11:50:58 +0200

    remove course example services

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,5 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import fetch from 'node-fetch';
-import { PublisherChannel } from './order-service/publisher-channel.js';
 
 const exampleData = {
   title: 'This is nice example',
@@ -18,59 +17,5 @@ export const mainRoute = (
   res.end();
 };
 
-// POST createOrder
-export const createOrder = (
-  req: IncomingMessage,
-  res: ServerResponse,
-  publisherChannel: PublisherChannel
-) => {
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk.toString();
-  });
-  req.on('end', async () => {
-    // Parse request body as JSON
-    const data = JSON.parse(body);
 
-    // Communicate with Inventory service:
-    // const inventoryResponse = await sendRequest(
-    //   "http://localhost:3001/api/inventory",
-    //   "PUT",
-    //   JSON.stringify(data)
-    // );
 
-    // Publish event:
-    await publisherChannel.sendEvent(body);
-
-    // console.log("My order:", data);
-    res.statusCode = 201;
-    res.end(
-      JSON.stringify({
-        data,
-      })
-    );
-  });
-};
-
-// PUT /api/inventory/
-export const updateItem = (req: IncomingMessage, res: ServerResponse) => {
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk.toString();
-  });
-  req.on('end', async () => {
-    // Parse request body as JSON
-    const data = JSON.parse(body);
-
-    console.log('FROM API CALL - My item:', data);
-
-    await fetch;
-
-    res.statusCode = 201;
-    res.end(
-      JSON.stringify({
-        data,
-      })
-    );
-  });
-};
