b7c67149486080941b50bc65b3aa6656dba1c62e
libby <libby@campus.technion.ac.il>
2024-03-17 13:32:55 +0200

    update user permission

--- src/gateway-service/gateway-service.ts
+++ src/gateway-service/gateway-service.ts
@@ -4,16 +4,19 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+app.use(express.json())
+
 const port = process.env.GATEWAY_PORT;
+const users_url = process.env.USERS_SERVICE_URL;
 
 
 
-app.get('/users', (req, res) => {
-  res.redirect('http://localhost:3002/users');
+app.post('/api/signup', (req, res) => {
+  res.redirect(`${users_url}/signup`);
+});
 
-  // req.url = '/api/user';
-  // next()
-  // res.send('Express + TypeScript Server');
+app.put('/api/permissions', (req, res) => {
+  res.redirect(`${users_url}/permissions`);
 });
 
 app.get('/events', (req, res) => {
