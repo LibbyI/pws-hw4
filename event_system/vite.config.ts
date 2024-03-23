db6154d27b74fc6e11dc917041f5cfcaf000e8a1
libby <libby@campus.technion.ac.il>
2024-03-23 19:03:54 +0200

    Edit build config for vite

--- event_system/vite.config.ts
+++ event_system/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/pws_hw4/event_system',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: 'mongoose'
+    }
+  },
 })
