cbda10a71202e30c5cbced6301ca28aa514f8b26
t-noaDickman <t-noaDickman@microsoft.com>
2024-03-28 15:34:06 +0200

    add fail payment for test

--- backend/src/orders-service/orders-concrete.ts
+++ backend/src/orders-service/orders-concrete.ts
@@ -133,8 +133,15 @@ export async function handlePaymentRequest(req) {
             await addNewOrder(req.body.order);  
         
 
-            const paymentDetails : paymentDetails = {charge: order.ticket.price * order.ticket.quantity, ...req.body.payment_details};
+
+            let paymentDetails : paymentDetails = {...req.body.paymentDetails as paymentDetails, charge: order.ticket.price * order.ticket.quantity};
+            if(paymentDetails.holder === "fail payment"){ 
+                //TODO: remove this! only for testing
+                console.log("fail payment");
+                throw new HttpError(500, "failed payment");
+            }
             paymentId = (await axios.post(PAYMENT_URL, paymentDetails)).data;
+        
             // console.log(paymentId.paymentToken, typeof paymentId.paymentToken);
 
             // console.log(order._id);
