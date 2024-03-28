e8e06b8a1cd22109269cd04a04a8b248e38d5461
t-noaDickman <t-noaDickman@microsoft.com>
2024-03-28 22:04:27 +0200

    personal space and refund buttons

--- event_system/src/header/header.tsx
+++ event_system/src/header/header.tsx
@@ -7,7 +7,7 @@ import { permissionValidTypes } from "../../../backend/src/models/user.ts";
 import {getUserById} from "../common/requests.ts";
 import { useNavigate, useParams } from 'react-router-dom';
 import { useState , useEffect } from 'react'
-import { getCookies } from '../common/utils.ts';
+import { getCookies, isBackoffice } from '../common/utils.ts';
 
 interface Props{
     logout: () => void;
@@ -20,7 +20,7 @@ export const Header: React.FC<Props> = ({logout, isLoggedIn}) => {
     return <></>;
   }
   const userId = getCookies("userId");
-  const permissionType = getCookies("permissionType");
+  const permissionType = getCookies("permissionType") as permissionValidTypes;
 
   const [user, setUser] = useState<any>(null); // Define user state
 
@@ -58,7 +58,17 @@ export const Header: React.FC<Props> = ({logout, isLoggedIn}) => {
           </Typography>
           <div style={{ flexGrow: 1 }} /> 
           {
-            [permissionValidTypes.Admin , permissionValidTypes.Manager].includes(permissionType as permissionValidTypes) ? <Button color="inherit" onClick={() => {navigate(`/newEvent`)}}>CREATE EVENT</Button> : <></>
+            [permissionValidTypes.Admin , permissionValidTypes.Manager].includes(permissionType) ?
+             <Button color="inherit" onClick={() => {navigate(`/newEvent`)}}>CREATE EVENT</Button> : 
+             <></>
+          }
+          {
+            isBackoffice(permissionType) ? <></> :
+            <>
+            <Button color="inherit" onClick={() => {navigate(`/personalSpace`)}}>PERSONAL SPACE</Button>
+            <Button color="inherit" onClick={() => {navigate(`/refund`)}}>Refund</Button>
+            </>
+
           }
           <Button color="inherit" onClick={() => {navigate(-1);}}>GO BACK</Button>
           <Button color="inherit" onClick={() => {logout();}}>LOGOUT</Button>
