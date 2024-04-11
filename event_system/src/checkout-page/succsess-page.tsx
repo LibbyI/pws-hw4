65901022fd92dfbafefc1bbf5e74a012ffb6b06c
t-noaDickman <t-noaDickman@microsoft.com>
2024-04-11 20:36:42 +0300

    fix

--- event_system/src/checkout-page/succsess-page.tsx
+++ event_system/src/checkout-page/succsess-page.tsx
@@ -1,6 +1,6 @@
 
 import { OrderSummary , OrderSummaryProps} from './order-summary'; // Replace '../path/to/OrderSummaryProps' with the actual path to the OrderSummaryProps file
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export interface SuccessPageProps {
@@ -18,10 +18,10 @@ const navigate = useNavigate();
             Congratulation! Enjoy!
         </Typography>
         <Typography variant='h2' component='div'>
-            Order Id: {orderId};
+            Order Id: {orderId}
         </Typography>
         <OrderSummary {...orderSummary}></OrderSummary>
-        <button onClick={() => navigate('../../catalog',{relative: "path"} )} >Go back to catalog</button>
+        <Button onClick={() => navigate('../../catalog',{relative: "path"} )} >Go back to catalog</Button>
         </Container>
     );
 }
